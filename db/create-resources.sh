18a0bbe58e7060a5abb06ae9d3f405b01768c15e
Ashton MacKenzie <pikeminnow@gmail.com>
2022-11-14 15:42:58 -0500

    migration script - all instances of belongs_to -> references

--- db/create-resources.sh
+++ db/create-resources.sh
@@ -3,6 +3,6 @@ rails g resource player name password_digest --no-test-framework
 rails g resource tama_character name hunger:integer attention:integer sick:boolean weight:float height:float --no-test-framework
 rails g resource sprite image_path --no-test-framework
 rails g resource Player_Has_Food player:references sprite:references quantity:integer --no-test-framework
-rails g resource Tama_Has_Sprite sprite:belongs_to tama_character:belongs_to --no-test-framework
-rails g resource Player_Friends_Tama player:belongs_to tama_character:belongs_to guestbook --no-test-framework
+rails g resource Tama_Has_Sprite sprite:references tama_character:references --no-test-framework
+rails g resource Player_Friends_Tama player:references tama_character:references guestbook --no-test-framework
 rails g resource Player_Owns_Tama player:references tama_character:references bio relationship:integer --no-test-framework
