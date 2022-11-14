9b66fb63cd98c81d273558269e0d36b9af67f834
Ashton MacKenzie <pikeminnow@gmail.com>
2022-11-14 11:06:39 -0500

    -after first deploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
@@ -16,23 +17,32 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Page Count: {count}</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Switch>
+            <Route path="/testing">
+              <h1>Test Route</h1>
+            </Route>
+            <Route path="/">
+              <h1>Page Count: {count}</h1>
+            </Route>
+          </Switch>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
