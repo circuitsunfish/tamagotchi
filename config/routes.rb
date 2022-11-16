1018786711ff385dda2cd1e4627448844e1e73b4
Ashton MacKenzie <pikeminnow@gmail.com>
2022-11-16 13:26:48 -0500

    clean up routes file on backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,26 @@
 Rails.application.routes.draw do
-  resources :player_owns_tamas
-  resources :player_friends_tamas
-  resources :gm_responses
-  resources :tama_has_sprites
-  resources :player_has_foods
-  resources :sprites
-  resources :tama_characters
-  resources :players
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :player_owns_tamas
-  resources :player_friends_tamas
-  resources :gm_responses
   resources :gm_responses, only: [:index, :show]
 
-
-#auth routes
-post "/signup", to: "users#create"
-post "/login", to: "sessions#create"
-delete "/logout", to: "sessions#destroy"
-
-    # route to test your configuration
-    get '/hello', to: 'application#hello_world'
-
+  resources :players
+  
+  resources :tama_characters
+  resources :player_owns_tamas
+  resources :tama_has_sprites
+  resources :player_has_foods
+  
+  #resources :sprites
+  resources :player_friends_tamas
+  
+  
+  
+  
+  #auth routes
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  
+  # # route to test your configuration
+  # get '/hello', to: 'application#hello_world'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
 end
