dbd59a95af726c125255e81d889e12ee11c807b0
Seth <seth.bbrendle@gmail.com>
2022-11-16 11:44:41 -0500

    tailwind

--- client/src/Footer.tsx
+++ client/src/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 
     return (
-        <div>
+        <div id="footer" className="">
             <p>Lovingly created by <a href="https://www.linkedin.com/in/ashton-mackenzie/">Ashton MacKenzie</a> and <a href="https://www.linkedin.com/in/seth-brendle/">Seth Brendle</a></p>
         </div>
     )
