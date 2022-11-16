bc0300c050dfc4ddd449ac8da4178a8dd8b85500
Ashton MacKenzie <pikeminnow@gmail.com>
2022-11-16 09:58:29 -0500

    adding handleLogout function

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -8,35 +8,36 @@ export default function Navbar() {
 
     function handleLogout() {
         fetch("/logout", {
-            method: "DELETE",
-        }).then(() => onLogout());
-    }
+            method: "DELETE",-
+        })
+    // .then(() => onLogout());
+}
 
 
 
-    return (
-        <nav>
-            <ul>
+return (
+    <nav>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/account">Account</Link>
+            </li>
+            <li>
+                <Link to="/save">Save</Link>
+            </li>
+            <li>
+                <Link to="/tamas">Visit Pets</Link>
+            </li>
+            {checkCookie('user_id') !== undefined ?
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link to="/logout">Logout</Link>
                 </li>
-                <li>
-                    <Link to="/account">Account</Link>
-                </li>
-                <li>
-                    <Link to="/save">Save</Link>
-                </li>
-                <li>
-                    <Link to="/tamas">Visit Pets</Link>
-                </li>
-                {checkCookie('user_id') !== undefined ?
-                    <li>
-                        <Link to="/logout">Logout</Link>
-                    </li>
-                    : null
-                }
+                : null
+            }
 
-            </ul>
-        </nav>
-    );
+        </ul>
+    </nav>
+);
 }
